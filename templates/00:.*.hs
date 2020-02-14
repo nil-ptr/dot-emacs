408e7a7f315f39faf0c6ca61ce5dadb3017535c0
Nils Gustafsson <nils.gustafsson@bredband2.com>
2020-02-14 07:56:48 +0100

    Snippets and templates

---
+++ templates/00:.*.hs
@@ -0,0 +1,20 @@
+${1:{-# LANGUAGE ${2:foo} #-}}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  $3
+-- Copyright   :  ${4:`(user-full-name)`} ${5:`(string-trim (shell-command-to-string "date +%Y"))`}
+-- License     :  ${6:${7:$$(yas-choose-value '("BSD-style" "Apache-2.0" "MIT"))} (see the LICENSE file in the distribution)}
+--
+-- Maintainer  :  ${8:`(identity user-mail-address)`}
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- ${9:Description forthcoming.}
+module $3
+  (
+  -- * Exported Items
+
+  ) where
+
+$0
